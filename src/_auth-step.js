1aa2a90329a61324b319bf100501330d4b0f81ca
unknown <islam15-3879@diu.edu.bd>
2022-04-13 15:30:59 +0600

    firebase-setting

---
+++ src/_auth-step.js
@@ -0,0 +1,22 @@
+/**
+ * 1: create a new project in console .google com
+ * 2: npm install
+ * 3:create fire . inti .js and import getauth to export auth 
+ * 4 : firebse setting go to authentication enable Email and Password auth
+ * 5:
+ * 
+ * 
+ * */ 
+
+
+
+
+/**
+ * 
+ * hosting step
+ * 1 npm install -g firebase tools
+ * 2 fire base login
+ * 3 firebase init 
+ * 4 firebase deploy
+ * 
+ * */
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-jUyhLaXUKvJkDoY8PuSJqkytVgbUXUE",
+  authDomain: "ema-john-simple-62641.firebaseapp.com",
+  projectId: "ema-john-simple-62641",
+  storageBucket: "ema-john-simple-62641.appspot.com",
+  messagingSenderId: "535106679340",
+  appId: "1:535106679340:web:ddd5f3d3c4cd3070b49789"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const auth=getAuth();
